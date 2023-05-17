0abfc32c00a0246ad31fa65555b3605ab94e85fd
ics2o-2-2022 <124388837+ics2o-2-2022@users.noreply.github.com>
2023-05-17 16:51:29 +0000

    update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-ics2o-2-2022/sw.js", {
+    scope: "/ICS2O-Unit-1-08-ics2o-2-2022/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
 function myButtonClicked() {
-  alert('Hello, World!')
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
 }
